034726cfc9998f85b255e5dc3bdfa1e68fa621c0
James Bebbington <james.bebbington@rokt.com>
2020-02-04 17:54:54 +1100

    correct tag names

--- lib/tag-release.sh
+++ lib/tag-release.sh
@@ -21,23 +21,23 @@ git config user.email "buildkite@rokt.com"
 git config user.name "Buildkite"
 
 # Delete any tags with name 'REMOVE_TAG_NAME' (might not exist)
-if [ "$REMOVE_TAG_NAME" != "" ]; then git push origin :refs/tags/"$REMOVE_TAG_NAME $env $region" || true; fi
+if [ "$REMOVE_TAG_NAME" != "" ]; then git push origin :refs/tags/"$REMOVE_TAG_NAME-$env-$region" || true; fi
 
 if [ "$REPLACE_TAGNAME" != "" ];
 then
     # Replace any tags with name 'CURRENT_TAG_NAME' with 'REPLACE_TAGNAME' (might not exist)
-    git push origin refs/tags/"$CURRENT_TAG_NAME $env $region":refs/tags/"$REPLACE_TAGNAME $env $region" :refs/tags/"$CURRENT_TAG_NAME $env $region" || true
+    git push origin refs/tags/"$CURRENT_TAG_NAME-$env-$region":refs/tags/"$REPLACE_TAGNAME-$env-$region" :refs/tags/"$CURRENT_TAG_NAME-$env-$region" || true
 else
     # Otherwise, delete any existing tags with name 'CURRENT_TAG_NAME' (might not exist)
-    git push origin :refs/tags/"$CURRENT_TAG_NAME $env $region" || true
+    git push origin :refs/tags/"$CURRENT_TAG_NAME-$env-$region" || true
 fi
 
-# Create new local tag
+# Create new local tag with name 'CURRENT_TAG_NAME'
 TAG_MESSAGE="{\"BUILDKITE_JOB_ID\"=${BUILDKITE_JOB_ID},\"BUILDKITE_BUILD_NUMBER\"=\"${BUILDKITE_BUILD_NUMBER}\",\"BUILDKITE_BUILD_URL\"=\"${BUILDKITE_BUILD_URL}\", }"
-git tag -fa "$CURRENT_TAG_NAME $env $region" -m "${TAG_MESSAGE}"
+git tag -fa "$CURRENT_TAG_NAME-$env-$region" -m "${TAG_MESSAGE}"
 
 # Show the tags
 git --no-pager tag
 
 # Push the new tag
-git push origin refs/tags/"$CURRENT_TAG_NAME $env $region"
+git push origin refs/tags/"$CURRENT_TAG_NAME-$env-$region"
